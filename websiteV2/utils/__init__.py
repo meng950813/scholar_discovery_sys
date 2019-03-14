ad0a5594a62b14eae058aa1061c788c6d4cdda1b
sky94520 <541052067@qq.com>
2019-03-14 20:56:29 +0800

    By <xiaoniu> 包含了实用功能，如根据关键字查询功能，简单封装了数据库的使用

---
+++ websiteV2/utils/__init__.py
@@ -0,0 +1,20 @@
+"""
+author: xiaoniu
+date: 2019-03-14
+desc: 实用工具包包含着一些常用的函数
+"""
+
+
+def db2python(results, key):
+    """
+    把数据库的list[dict]格式转为dict[id]={}格式，仅仅适用于key唯一的情况
+    :param results: 从数据库中查询得到的数据列表 数据项格式为dict
+    :param key: 主键 目前仅仅支持int或者str
+    :return: 返回转换后的dict
+    """
+    d = {}
+    for result in results:
+        result_id = result[key]
+        result.pop(key)
+        d[result_id] = result
+    return d
