705cc6d55796c8838daf2a7656a407f4701387ca
meng950813 <1752397568@qq.com>
2019-03-15 13:51:22 +0800

    by chen: modified the router index to basic.html

--- websiteV2/app.py
+++ websiteV2/app.py
@@ -4,13 +4,14 @@ from utils import db
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint)
+
 # 需要预先调用，且只调用一次
-db.create_engine('root', '9527', 'training')
+db.create_engine('db_user', 'pwd', 'db_name')
 
 
 @app.route('/')
 def index():
-    return render_template("map.html")
+    return render_template("basic.html")
 
 
 if __name__ == '__main__':
