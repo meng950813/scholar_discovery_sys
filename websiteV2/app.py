f795a10d6949c7d0f1d318096571a061e7a036d4
sky94520 <541052067@qq.com>
2019-03-15 19:20:52 +0800

    By <xiaoniu> 添加了config配置dict的使用

--- websiteV2/app.py
+++ websiteV2/app.py
@@ -1,12 +1,13 @@
 from flask import Flask,render_template
 from controllers.api import api_blueprint
 from utils import db
+from config import DB_CONFIG
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint)
 
 # 需要预先调用，且只调用一次
-db.create_engine('db_user', 'pwd', 'db_name')
+db.create_engine(**DB_CONFIG)
 
 
 @app.route('/')
